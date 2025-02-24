94b69bc8c91ca5c804bb816437ac6b0009cead5d
Jade Harley <jade@witchof.space>
2025-02-24 17:57:02 -0500

    Updated to 0.29 claude-code

--- claude-code/default.nix
+++ claude-code/default.nix
@@ -1,17 +1,17 @@
 { lib, buildNpmPackage, fetchurl }:
 buildNpmPackage rec {
   pname = "claude-code";
-  version = "0.2.8";
+  version = "0.2.9";
 
   src = fetchurl {
     url = "https://registry.npmjs.org/@anthropic-ai/${pname}/-/${pname}-${version}.tgz";
-    hash = "sha256-ZhIW3W2lTqNTb+upS3VUr4ojnWdgLFSkdHmH8m6p2us="; # Replace with actual hash
+    hash = "sha256-/WBysiuds6ZwwSSUFDr+sGHgRYCyFhH6bEai+XxHsYw=";
   };
   dontNpmBuild = true;
   postPatch = ''
     cp ${./package-lock.json} ./package-lock.json
   '';
-  npmDepsHash = "sha256-sNbATV3SQFOWYRISWK71dE7+P2YD7EW9Xmmq8gUDQFs=";
+  npmDepsHash = "sha256-2v9wCcaOgA3RezX/pnqigsn6XhKcqP2adM2IGRhiHgc=";
   AUTHORIZED = "1";
   meta = with lib; {
     description = "Agentic coding tool that lives in your terminal, understands your codebase, and helps you code faster by executing routine tasks, explaining complex code, and handling git workflows - all through natural language commands.";
